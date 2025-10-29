254c1b3c93e498f2e34dae5ef2bab710aec21e0c
Rahat Hossain <rahat1470.com@gmail.com>
2025-10-29 16:53:33 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,8 @@ import { FlipWords } from "./components/ui/flip-words";
 import { CardSpotlight } from "./components/ui/card-spotlight";
 
 import { SparklesCore } from "./components/ui/sparkles";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+import { Card, CardContent, CardFooter } from "./components/ui/card";
 import { ShineBorder } from "./components/ui/shine-border";
-import { Label } from "./components/ui/label";
-import { Input } from "./components/ui/input";
 export default function App() {
   const navItems = [
     {
